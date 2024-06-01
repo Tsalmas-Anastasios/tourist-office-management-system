3ee209ec111923f3bb1a56b2f249a624f724eff9
Tsalmas Anastasios <tsalmanastasios@gmail.com>
2024-06-01 14:36:40 +0300

    initial commit

---
+++ src/main/java/com/tourist_office_management_system/tourist_office_management_system/TouristOfficeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tourist_office_management_system.tourist_office_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristOfficeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristOfficeManagementSystemApplication.class, args);
+	}
+
+}
